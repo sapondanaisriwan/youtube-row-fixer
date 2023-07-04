c0be1edce8d32c2b650e76c47bfb075d5dbd853e
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2023-07-04 18:54:43 +0700

    Update: v1.0.1

--- userscript/yt-videos-per-row-fix.user.js
+++ userscript/yt-videos-per-row-fix.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name        YouTube Videos Per Row Fix
-// @version     1.0.0
+// @name        YouTube Row Fixer
+// @version     1.0.1
 // @author      sapondanaisriwan
 // @description Increase the videos per row on all page.
 // @match       https://www.youtube.com/*
@@ -11,7 +11,7 @@
 // @namespace   https://greasyfork.org/en/users/1021085-sapondanaisriwan
 // @homepageURL https://github.com/sapondanaisriwan/youtube-videos-per-row-fix
 // @supportURL  https://github.com/sapondanaisriwan/youtube-videos-per-row-fix/issues
-// @require     https://greasyfork.org/scripts/465819-api-for-customelements-in-youtube/code/API%20for%20CustomElements%20in%20YouTube.js?version=1187694
+// @require     https://greasyfork.org/scripts/465819-api-for-customelements-in-youtube/code/API%20for%20CustomElements%20in%20YouTube.js?version=1215125
 // ==/UserScript==
 
 /*
@@ -24,9 +24,68 @@ Support me: https://ko-fi.com/sapondanaisriwan
 Support me: https://ko-fi.com/sapondanaisriwan
 */
 
-'use strict';
+"use strict";
 
 // Thanks so much to CY Fung (https://greasyfork.org/en/users/371179-cy-fung) for helping me.
-const videosPerRow = 5
-const target = 'ytd-rich-grid-renderer'
-customYtElements.whenRegistered(target, (proto) => { proto.calcElementsPerRow = () => videosPerRow; });
+const settings = {
+  VIDEO_PER_ROW: 6,
+  SHELF_ITEM_PER_ROW: 12, // Max is 12
+  HIDE_PROFILE: true,
+};
+
+const target = "ytd-rich-grid-renderer";
+
+const styles = {
+  hideProfile: {
+    id: "hide-ch-profile",
+    css: `
+    .channel-avatar.ytd-ghost-grid-renderer,
+    #home-page-skeleton .channel-avatar {
+        display: none;
+    }
+  
+    ytd-rich-grid-media a#avatar-link {
+        display: none;
+    }
+    `,
+  },
+  skeletonEle: {
+    id: "unload-videos",
+    css: `
+    #home-page-skeleton .rich-grid-media-skeleton,
+    #home-page-skeleton .rich-shelf-videos .rich-grid-media-skeleton.mini-mode,
+    #home-page-skeleton #home-container-media .rich-grid-media-skeleton.mini-mode {
+      min-width: calc(100% / ${settings.VIDEO_PER_ROW} - 1.6rem);
+      max-width: calc(100% / ${settings.VIDEO_PER_ROW} - 1.6rem);
+    }
+    `,
+  },
+};
+
+// Function to remove DOM element
+const removeEle = (id) => {
+  const ele = document.getElementById(id);
+  ele && ele.remove();
+};
+
+// Function to inject a style into the webpage
+const injectStyle = (id, css) => {
+  // Remove before adding
+  removeEle(id);
+
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.id = id;
+  style.textContent = css;
+  document.documentElement.appendChild(style);
+};
+
+injectStyle(styles.skeletonEle.id, styles.skeletonEle.css);
+settings.HIDE_PROFILE &&
+  injectStyle(styles.hideProfile.id, styles.hideProfile.css);
+
+customYtElements.whenRegistered(target, (proto) => {
+  proto.calcElementsPerRow = (a, b) => {
+    return a === 194 ? settings.SHELF_ITEM_PER_ROW : settings.VIDEO_PER_ROW;
+  };
+});
