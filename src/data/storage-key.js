4d868a034f607708e1faf1edc881084c1eae432f
Adashima <sapondanaisriwan@gmail.com>
2024-05-07 13:23:57 +0700

    Update: storage-key.js

--- src/data/storage-key.js
+++ src/data/storage-key.js
@@ -4,6 +4,7 @@ export const KeyExtensionTheme = "extensionTheme";
 
 export const KeyHideChannelProfile = "hideChannelProfile";
 export const KeyDisplayFullTitle = "displayFullTitle";
+export const KeyDynamicVideo = "dynamicVideoPerRow";
 export const KeyVideoPerRow = "videoPerRow";
 export const KeyPostPerRow = "postPerRow";
 export const KeyShelfItemPerRow = "shelfItemPerRow";
@@ -11,6 +12,8 @@ export const KeyShelfItemPerRow = "shelfItemPerRow";
 export const settingKey = [
   KeyExtensionStatus,
   KeyExtensionTheme,
+
+  KeyDynamicVideo,
   KeyHideChannelProfile,
   KeyDisplayFullTitle,
   KeyVideoPerRow,
@@ -22,6 +25,7 @@ export const defaultSetting = {
   [KeyExtensionStatus]: true,
   [KeyExtensionTheme]: true,
 
+  [KeyDynamicVideo]: false,
   [KeyHideChannelProfile]: true,
   [KeyDisplayFullTitle]: false,
   [KeyVideoPerRow]: 5,
