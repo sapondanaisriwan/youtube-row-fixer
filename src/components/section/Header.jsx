593feb72cdc137a5dcef9c6623368cde17056ed3
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-04-17 20:50:33 +0700

    fix: add donate button and heart icon to the Header component

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,4 +1,4 @@
-import { Navbar, NavbarBrand, NavbarContent } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, Button } from "@nextui-org/react";
 import ExtensionSetting from "./ExtensionSetting";
 import Logo from "../icon/logo";
 
@@ -18,10 +18,39 @@ function Header() {
         </a>
       </NavbarBrand>
       <NavbarContent justify="end">
+        <a
+          href="https://ko-fi.com/sapondanaisriwan"
+          target="_blank"
+        >
+          <Button size="sm" startContent={<HeartIcon />} aria-label="Like" variant="bordered" radius="full">
+            <p className="select-none">Donate</p>
+          </Button>
+        </a>
         <ExtensionSetting />
       </NavbarContent>
     </Navbar>
   );
 }
+
+export const HeartIcon = ({ fill = "currentColor", filled, size, height, width, ...props }) => {
+  return (
+    <svg
+      fill={filled ? fill : "none"}
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12.62 20.81c-.34.12-.9.12-1.24 0C8.48 19.82 2 15.69 2 8.69 2 5.6 4.49 3.1 7.56 3.1c1.82 0 3.43.88 4.44 2.24a5.53 5.53 0 0 1 4.44-2.24C19.51 3.1 22 5.6 22 8.69c0 7-6.48 11.13-9.38 12.12Z"
+        stroke={fill}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={1.5}
+      />
+    </svg>
+  );
+};
 
 export default Header;
