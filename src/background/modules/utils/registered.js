20823067d21fc174a76a12881939c89cd357789a
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2025-05-20 21:39:57 +0700

    refactor: add error handling to script registration and unregistration functions

--- src/background/modules/utils/registered.js
+++ src/background/modules/utils/registered.js
@@ -1,26 +1,38 @@
 import { allScriptIds } from "../../../data/scriptId";
 
 export const getRegisteredScripts = async () => {
-  const matchingScripts = await chrome.scripting.getRegisteredContentScripts({
-    ids: allScriptIds,
-  });
-  return matchingScripts.length > 0;
+  try {
+    const matchingScripts = await chrome.scripting.getRegisteredContentScripts({
+      ids: allScriptIds,
+    });
+    return matchingScripts.length > 0;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const injectScript = async ({ id, runAt, world, files }) => {
-  await chrome.scripting.registerContentScripts([
-    {
-      id: id,
-      runAt: runAt ?? "document_start", // document_end, document_idle, document_start
-      world: world ?? "ISOLATED", // MAIN, ISOLATED
-      matches: ["https://www.youtube.com/*"],
-      js: files,
-    },
-  ]);
+  try {
+    await chrome.scripting.registerContentScripts([
+      {
+        id: id,
+        runAt: runAt ?? "document_start", // document_end, document_idle, document_start
+        world: world ?? "ISOLATED", // MAIN, ISOLATED
+        matches: ["*://*.youtube.com/*"],
+        js: files,
+      },
+    ]);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const unregisterScripts = async (id) => {
-  await chrome.scripting.unregisterContentScripts({
-    ids: id,
-  });
+  try {
+    await chrome.scripting.unregisterContentScripts({
+      ids: id,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
